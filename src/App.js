f0f3646562d90374cf6593b07bc0f9ac3be13913
Enshinyan <emeka.enshinyan@gmail.com>
2021-10-16 22:23:27 -0700

    creating searchbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 //the react functional component with jsx code (js html code, an extension of js). 
   //Often used to make react code simpler to use
-
-import './App.css';
-import Appbar from './components/AppBar';
-function App() {
-  return (
-    <div className="App">
-    <Appbar/>
-    </div>
-  );
-}
-export default App;
+  import * as React from 'react';
+  //import ReactDOM from 'react-dom';
+  //import Button from '@mui/material/Button';
+  import './App';
+import PrimarySearchAppBar from './components/appbar';
+// after from may be wrong call
+  
+  //function App() {
+    const App = () => {
+    return (
+      <div className="App">
+        <PrimarySearchAppBar/>
+      </div>
+    );
+  }
+  export default App;
+  //ReactDOM.render(<App />, document.querySelector('#app'));
+  
