55befe1c0867db21c7b2e473333733344537df51
Enshinyan <emeka.enshinyan@gmail.com>
2021-10-16 20:59:08 -0700

    added mui appbar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 //the react functional component with jsx code (js html code, an extension of js). 
   //Often used to make react code simpler to use
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Appbar from './components/AppBar';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This Atom Has 3 Electrons
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Appbar/>
     </div>
   );
 }
-export default function BasicButtons() {
-  return (
-    <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-    </Stack>
-  );
-}
+export default App;
