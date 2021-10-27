b72706402e39f0ee036cbdd21dd85d795d36f4cc
Enshinyan <emeka.enshinyan@gmail.com>
2021-10-27 16:51:44 -0700

    making user/article data

---
+++ src/Components/UserComponent.js
@@ -0,0 +1,22 @@
+import React from "react";
+import UserService from "./Service/UserService";
+
+class UserComponent extends React.Component{
+
+    //??
+    //the state defines a users array
+    constructor (){
+        this.state = {users:[]}
+    }
+    //creates componnent that will display a list of usersm 
+    render (){
+        return (<div>
+
+        </div>)
+    }
+
+}
+
+//need to export class
+
+export default UserComponent;
--- src/Service/UserService.js
+++ src/Service/UserService.js
@@ -1,3 +1,14 @@
 import axios from 'axios'
 
-const USERS_RST_API_URL = 'http://localhost:8080/api/';
+const USERS_REST_API_URL = 'http://localhost:8080/api/test/article';
+
+class UserService{
+//here get the userservice class
+    getUsers(){
+        axios.get(USERS_REST_API_URL);
+    }
+}
+
+//export object for class
+
+export default new UserService();
