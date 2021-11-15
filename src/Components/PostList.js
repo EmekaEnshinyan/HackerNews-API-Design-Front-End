8230afb3b10af35054f74f25a531690e420263be
Emeka <emeka.enshinyan@gmail.com>
2021-11-15 02:41:03 -0800

    edit: remove wds wait error

--- src/Components/PostList.js
+++ src/Components/PostList.js
@@ -6,13 +6,13 @@ export class PostList extends Component {
     constructor(props){
         super(props)
         this.state = {
-            posts: []
+            posts: JSON
         }
         
     }
-
+//https://hacker-news.firebaseio.com/v0/item/29042728.json?print=pretty
     componentDidMount(){
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://hacker-news.firebaseio.com/v0/item/29042728.json?print=pretty')
         .then(response => {
             console.log(response)
             this.setState({posts: response.data})
@@ -30,7 +30,7 @@ export class PostList extends Component {
                 List Below
                 {
                 posts.length ? 
-                posts.map(post => <div key={post.id}>{post.title}</div>):
+                posts.map(post => <div key={post.id}>{post.by}</div>):
                 null
                 }
             </div>
